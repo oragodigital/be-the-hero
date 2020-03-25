562ab492b61ba906cbe769ba3b6ade84d538f91e
Rafael Douglas <rafael@orago.com.br>
2020-03-25 00:32:16 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express =  require('express');
+const cors =  require('cors');          
+const routes = require('./routes');
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // Define que vamos trabalhar com requisições json
+app.use(routes);
+
+app.listen(3333);
+
+    
