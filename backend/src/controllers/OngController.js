68689afdbd6a470f89b55ae95d8190e668af025c
Rafael Douglas <rafael@orago.com.br>
2020-03-27 01:39:12 -0300

    Front-end do app

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -6,6 +6,7 @@ module.exports = {
         const ongs = await connection('ongs').select('*');
         return response.json(ongs);
     },
+
     async create (request, response){
 
         const { name, email, whatsapp, city, uf } = request.body;
@@ -20,6 +21,5 @@ module.exports = {
             uf
         });
         return response.json({ id });
-
     }
 }
